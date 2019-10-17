ea1ec6d417c13a9c7d817d90dc9e0962f9b9f85f
Damandeep Singh <mail@damandeepsingh.com>
2019-10-17 21:17:03 +0530

    removed unused imports

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const app = express();
-
 app.use(express.static('src'));
-
-
-// module.exports = app;
-const http = require('http');
-
-const server = http.createServer(app);
-
 app.listen(3001, function() {
   console.log(`Server is listening on port 3001\nProxy available on 3002`);
 });
